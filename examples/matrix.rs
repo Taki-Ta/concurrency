8f26f5116500e5e5b1253d6336bc1cc924cfcd03
Taki-Ta <taki184344812@outlook.com>
2024-05-02 12:31:30 +0800

    refactor:add vector struct to refact matrix

--- examples/matrix.rs
+++ examples/matrix.rs
@@ -4,7 +4,7 @@ use concurrency::{multiply, Matrix};
 fn main() -> Result<()> {
     let a = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
     let b = Matrix::new(vec![1, 2, 3, 4, 5, 6], 3, 2);
-    let c = multiply(a, b);
+    let c = multiply(a, b)?;
     println!("c={}", c);
     Ok(())
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,5 @@
+use crate::Vector;
+use anyhow::Result;
 use std::{
     fmt::{Debug, Display},
     ops::{Add, AddAssign, Mul},
@@ -7,34 +9,55 @@ use std::{
 #[allow(dead_code)]
 pub struct Matrix<T> {
     data: Vec<T>,
-    rows: usize,
-    cols: usize,
+    row: usize,
+    col: usize,
 }
 
 impl<T> Matrix<T> {
-    pub fn new(data: impl Into<Vec<T>>, rows: usize, cols: usize) -> Self {
+    pub fn new(data: impl Into<Vec<T>>, row: usize, col: usize) -> Self {
         Matrix {
             data: data.into(),
-            rows,
-            cols,
+            row,
+            col,
         }
     }
 }
 
-pub fn multiply<T>(a: Matrix<T>, b: Matrix<T>) -> Matrix<T>
+pub fn multiply<T>(a: Matrix<T>, b: Matrix<T>) -> Result<Matrix<T>>
 where
     T: Copy + Default + Add<Output = T> + Mul<Output = T> + AddAssign,
 {
-    assert_eq!(a.cols, b.rows);
-    let mut data = vec![T::default(); a.rows * b.cols];
-    for i in 0..a.rows {
-        for j in 0..b.cols {
-            for k in 0..a.cols {
-                data[i * b.cols + j] += a.data[i * a.cols + k] * b.data[k * b.cols + j];
-            }
+    assert_eq!(a.col, b.row);
+    let mut data = vec![T::default(); a.row * b.col];
+    for i in 0..a.row {
+        for j in 0..b.col {
+            let a_part = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let b_data = b
+                .data
+                .iter()
+                .skip(j)
+                .step_by(b.col)
+                .cloned()
+                .collect::<Vec<_>>();
+            let b_part = Vector::new(b_data);
+            data[i * b.col + j] = dot_product(a_part, b_part)?;
         }
     }
-    Matrix::new(data, a.rows, b.cols)
+    Ok(Matrix::new(data, a.row, b.col))
+}
+
+pub fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> Result<T>
+where
+    T: Default + Mul<Output = T> + AddAssign + Copy,
+{
+    if a.len() != b.len() {
+        return Err(anyhow::anyhow!("dot product error: a.len()!=b.len()"));
+    }
+    let mut res = T::default();
+    for i in 0..a.len() {
+        res += a[i] * b[i];
+    }
+    Ok(res)
 }
 
 impl<T> Display for Matrix<T>
@@ -44,14 +67,14 @@ where
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         //show [1,2,3,4,5,6] 2,3 as {1 2 3,4 5 6} and show [1,2,3,4,5,6] 3,2 as {1 2,3 4,5 6}
         write!(f, "{{")?;
-        for i in 0..self.rows {
-            for j in 0..self.cols {
-                write!(f, "{}", self.data[i * self.cols + j])?;
-                if j < self.cols - 1 {
+        for i in 0..self.row {
+            for j in 0..self.col {
+                write!(f, "{}", self.data[i * self.col + j])?;
+                if j < self.col - 1 {
                     write!(f, " ")?;
                 }
             }
-            if i < self.rows - 1 {
+            if i < self.row - 1 {
                 write!(f, ",")?;
             }
         }
@@ -65,24 +88,23 @@ where
     T: Display,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "Matrix:rows={},cols={},{}",
-            &self.rows, &self.cols, &self
-        )
+        write!(f, "Matrix:rows={},cols={},{}", &self.row, &self.col, &self)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Result;
+
     use super::*;
     #[test]
-    fn test_matrix() {
+    fn test_matrix() -> Result<()> {
         let a = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
         let b = Matrix::new(vec![1, 2, 3, 4, 5, 6], 3, 2);
-        let c = multiply(a, b);
-        assert_eq!(c.cols, 2);
-        assert_eq!(c.rows, 2);
+        let c = multiply(a, b)?;
+        assert_eq!(c.col, 2);
+        assert_eq!(c.row, 2);
         assert_eq!(format!("{:?}", c), "Matrix:rows=2,cols=2,{22 28,49 64}");
+        Ok(())
     }
 }
